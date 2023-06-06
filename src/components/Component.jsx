071cf96d1b6942fd7452ebab0b2963f1b376de4a
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-06 21:59:25 +0530

    map data to card

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -1,8 +1,11 @@
 import data from '../data/data'
+import Card from './Card'
 
 const Component = () => {
     return (
-        <section></section>
+        <section>
+            {data.map(ele => (<Card image={ele.image} title={ele.title} desc={ele.desc} key={ele.title} />))}
+        </section>
     )
 }
 
