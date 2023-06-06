bcaaca181795e78d1724bec586505d9fa5c28362
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-06 22:23:18 +0530

    adding color theme

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -2,8 +2,13 @@ import data from '../data/data'
 import Card from './Card'
 
 const Component = () => {
+
+    const sedanTheme = "bg-[#e38826]"
+    const suvTheme = "bg-[#e38826]"
+    const luxuryTheme = "bg-[#e38826]"
+
     return (
-        <section className='flex'>
+        <section className='flex max-w-[920px]'>
             {data.map(ele => (<Card image={ele.image} title={ele.title} desc={ele.desc} key={ele.title} />))}
         </section>
     )
