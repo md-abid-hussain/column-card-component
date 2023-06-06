ac741c6947da9af2cae8fbb221576d9a016ad34e
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-06 22:26:06 +0530

    added theme to data

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,25 @@
 import luxury from '../assets/icon-luxury.svg'
 import sedans from '../assets/icon-sedans.svg'
 import suvs from '../assets/icon-suvs.svg'
+
 const data = [
     {
         image: sedans,
         title: 'Sedans',
-        desc: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
+        desc: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+        theme: "#e38826"
     },
     {
         image: suvs,
         title: 'SUVs',
-        desc: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
+        desc: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+        theme: "#e38826"
     },
     {
         image: luxury,
         title: 'Luxury',
-        desc: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
+        desc: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+        theme: "#e38826"
     }
 ]
 
