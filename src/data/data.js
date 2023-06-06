c05ac89b17bc122c8aaa6742d17400ad420e34bc
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-06 21:47:14 +0530

    added icons to data

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,19 @@
-
+import luxury from '../assets/icon-luxury.svg'
+import sedans from '../assets/icon-sedans.svg'
+import suvs from '../assets/icon-suvs.svg'
 const data = [
     {
+        image: sedans,
         title: 'Sedans',
         desc: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
     },
     {
+        image: suvs,
         title: 'SUVs',
         desc: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
     },
     {
+        image: luxury,
         title: 'Luxury',
         desc: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
     }
