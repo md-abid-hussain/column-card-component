21e4d0bd6cb5832fa6c993f344d571346b30a284
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-10 11:46:02 +0530

    added colors to component according to style guide

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,15 @@ const Card = ({ image, title, desc, theme }) => {
 
     return (
         <section
-            className={`p-[48px] text-white flex-1
+            className={`p-[48px] text-[#f2f2f2] flex-1
              ${title === 'sedans' ? 'sedans' : title === 'suvs' ? 'suvs' : title === 'luxury' ? 'luxury' : ''}`
             }
         >
             <img src={image} />
-            <h2 className="uppercase text-[36px] mb-[36px] mt-[40px] text-white">{title}</h2>
+            <h2 className="uppercase text-[36px] mb-[36px] mt-[40px] text-[#ffffffbf]">{title}</h2>
             <p className="mb-[86px] text-white">{desc}</p>
             <a href="#"
-                className={`bg-white px-[28px] py-[14px] rounded-full border-4 border-[white] hover:text-[white] 
+                className={`bg-[#f2f2f2] px-[28px] py-[14px] rounded-full border-4 border-[#f2f2f2] hover:text-[#f2f2f2] 
                 ${title === 'sedans' ? 'hover:bg-[#e38826] text-[#e38826]' : title === 'suvs' ? 'hover:bg-[#006970] text-[#006970]' : title === 'luxury' ? 'hover:bg-[#004241] text-[#004241]' : ''}                
                 `}
             >Learn More</a>
