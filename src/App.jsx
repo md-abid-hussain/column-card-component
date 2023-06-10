110147c178eeedada9e8a724ebabf4c2f9f694e6
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-10 11:45:24 +0530

    added color given in style guide to background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Component from "./components/Component"
 function App() {
   return (
-    <main className="min-h-screen grid place-content-center p-4">
+    <main className="min-h-screen grid place-content-center p-4 bg-[#f2f2f2]">
       <Component />
     </main>
   )
