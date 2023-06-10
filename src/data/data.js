5c22e185de6100b0333cc50ea16390738c0443f4
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-10 11:50:05 +0530

    removed unused item in data

--- src/data/data.js
+++ src/data/data.js
@@ -6,20 +6,17 @@ const data = [
     {
         image: sedans,
         title: 'sedans',
-        desc: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
-        theme: "#e38826"
+        desc: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
     },
     {
         image: suvs,
         title: 'suvs',
-        desc: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
-        theme: "#006970"
+        desc: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
     },
     {
         image: luxury,
         title: 'luxury',
-        desc: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
-        theme: "#004241"
+        desc: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
     }
 ]
 
