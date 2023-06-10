7996f9154100e6d290fa8ca3c59edc87f04a7182
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-10 11:47:16 +0530

    fixed the color of heading and paragraph

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,8 +7,8 @@ const Card = ({ image, title, desc, theme }) => {
             }
         >
             <img src={image} />
-            <h2 className="uppercase text-[36px] mb-[36px] mt-[40px] text-[#ffffffbf]">{title}</h2>
-            <p className="mb-[86px] text-white">{desc}</p>
+            <h2 className="uppercase text-[36px] mb-[36px] mt-[40px] text-[#f2f2f2]">{title}</h2>
+            <p className="mb-[86px] text-[#ffffffbf]">{desc}</p>
             <a href="#"
                 className={`bg-[#f2f2f2] px-[28px] py-[14px] rounded-full border-4 border-[#f2f2f2] hover:text-[#f2f2f2] 
                 ${title === 'sedans' ? 'hover:bg-[#e38826] text-[#e38826]' : title === 'suvs' ? 'hover:bg-[#006970] text-[#006970]' : title === 'luxury' ? 'hover:bg-[#004241] text-[#004241]' : ''}                
